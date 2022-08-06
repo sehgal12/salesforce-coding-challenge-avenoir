063884893c768222625fef47cb983e08fec5edb6
DolBy <ashish.avenoir@gmail.com>
2022-08-06 16:43:57 +0530

    first commit

--- force-app/main/default/lwc/toast_AccountHealth/toast_AccountHealth.js
+++ force-app/main/default/lwc/toast_AccountHealth/toast_AccountHealth.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import daysPassed from '@salesforce/schema/Account.Days__c';
+import getDays from '@salesforce/apex/RestaurantLocationTriggerHandler.getDays';
 
-export default class toast_AccountHealth extends LightningElement {}
+export default class toast_AccountHealth extends LightningElement {
+ red = false;
+ green = true;
+ yellow = false; 
+ connectedCallback() {
+   this.getLabel();
+ }
+
+ @wire(getDays) days; 
+ getLabel(){
+        if (days > 0) {
+            if (days < 30) {
+                this.green = true;
+            }
+            else if (30 < days < 60) {
+                this.yellow = true
+            }
+            else if (60 < days) {
+                this.red = true;
+            }
+        } 
+    } 
+}
